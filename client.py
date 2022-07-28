5aea87087b1caf058d187f0db0feda89c89ac642
ChtobiVamBuloSlojneeZaponitGitMuPobedim <annonim147@mail.ru>
2022-07-28 11:54:07 +0300

    A running server with an endless request from the client for the title of the book or the name of the author and receiving a response from the server from the database

--- client.py
+++ client.py
@@ -8,23 +8,27 @@ def run():
     print("Press 'Enter' to exit")
     name = input("Enter the name of the author or the title of the book: ")
     while len(name) != 0:
-        with grpc.insecure_channel('localhost:8080') as channel:
-            stub = proto_pb2_grpc.GreeterStub(channel)
-            response = stub.Library(proto_pb2.Request(name=name))
+        if name == 'clear':
+            for i in range(10):
+                print()
+        else:
+            with grpc.insecure_channel('localhost:8080') as channel:
+                stub = proto_pb2_grpc.GreeterStub(channel)
+                response = stub.Library(proto_pb2.Request(name=name))
+                print()
+                if response.message[0] == '0':
+                    print('There is no such book or author in the database.')
+                if response.message[0] == '1':
+                    array_books = response.message[2:].split()
+                    print(name + ":")
+                    for name_book in array_books:
+                        print(name_book)
+                if response.message[0] == '2':
+                    print(response.message[2:] + ":")
+                    print(name)
             print()
-            if response.message[0] == '0':
-                print('There is no such book or author in the database.')
-            if response.message[0] == '1':
-                array_books = response.message[2:].split()
-                print(name + ":")
-                for name_book in array_books:
-                    print(name_book)
-            if response.message[0] == '2':
-                print(response.message[2:] + ":")
-                print(name)
-        print()
-        print("Press 'Enter' to exit")
-        name = input("Enter the name of the author or the title of the book: ")
+            print("Press 'Enter' to exit")
+            name = input("Enter the name of the author or the title of the book: ")
 
 
 run()
